35e6c05aad5b7a3a3409da486c1b93bb93ba8f78
hobeats <hobeats@gmail.com>
2024-11-02 09:36:46 +0900

    theme-toggle

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import { darktheme, lighttheme} from "./theme";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient()
 
+const Root = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false); 
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev); 
+  };
+
+  return (
+    <ThemeProvider theme={isDarkMode ? darktheme : lighttheme}>
+      <button onClick={toggleTheme}>
+        {isDarkMode ? '라이트모드' : '다크모드'}
+      </button>
+      <App />
+    </ThemeProvider>
+  );
+};
+
 ReactDOM.render(
+  <React.StrictMode>
     <QueryClientProvider client ={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>,
+      <Root/>
+    </QueryClientProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,13 @@
 import { DefaultTheme } from "styled-components";
-export const theme: DefaultTheme = {
+export const darktheme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#f5f6fa",
   accentColor: "#8c7ae6",
 };
+
+export const lighttheme: DefaultTheme = {
+  textColor : "#000000",
+  bgColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
+};
+
