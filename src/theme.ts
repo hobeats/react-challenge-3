880e6c02b7b5fba61e8f795185b3d3f8c471e055
jeho <hobeats@gmail.com>
2024-11-07 09:30:02 +0900

    animation

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,3 @@
 import { DefaultTheme } from "styled-components";
-export const darktheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "#8c7ae6",
-  disabledColor: "ffffff",
-};
 
-export const lighttheme: DefaultTheme = {
-  textColor : "#181C14",
-  bgColor: "#FEF9F2",
-  accentColor: "#8c7ae6",
-  disabledColor: "EDE8DC",
-};
-
+export const darkTheme: DefaultTheme = {};
