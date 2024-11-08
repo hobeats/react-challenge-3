45b4c500302cf3fad49c819d0edbdedb4ff20e0a
jeho <hobeats@gmail.com>
2024-11-08 11:22:00 +0900

    animate2

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,33 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { rotateZ: 360, scale: 1, transition: { type: "spring", delay: 0.5 } },
+const boxVar = {
+  hover: { scale: 1.5, rotateZ: -90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgba(155, 89, 182,1.0)",
+    transition:{
+      duration:10
+    }
+   }
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end"/>
+      <Box
+        drag
+        variants={boxVar}
+        whileDrag="drag"
+        whileHover="hover"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
